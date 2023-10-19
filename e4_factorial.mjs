489cd401caa3d5ead3bf52b2d108a4dc4101eaff
jente_tav <54480755+jente01t@users.noreply.github.com>
2023-10-19 10:43:54 +0200

    Update e4_factorial.mjs

---
+++ e4_factorial.mjs
@@ -0,0 +1,22 @@
+import * as readline from 'node:readline/promises';
+import{stdin as input, stdout as output} from 'node:process';
+const userInput = readline.createInterface({input, output});
+
+let getal = parseFloat(await userInput.question('Geef een getal: '));
+let uitkomst = 1;
+
+if (getal > 0) {
+    factorial (getal);
+} else {
+    console.error("fout");
+}
+
+
+function factorial (getal) {
+    for (let i = 1; i <= getal; i++) {
+        uitkomst = uitkomst * i
+    } console.log(uitkomst);
+    return uitkomst;
+}
+
+process.exit();
